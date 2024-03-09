1ffacfec1c4fbd86d2114e4df6dea47fff58b7bb
OOOCIN <Nicolas_baquing@rcc.edu.ph>
2024-03-09 15:42:07 +0800

    addition

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form action="math.php" method="post">
+        input1: <input type="text" name="input1"><br>
+        input2: <input type="text" name="input2"><br>
+        <input type="submit">
+        </form>
+    </body>
+</html>
---
+++ math.php
@@ -0,0 +1,7 @@
+<?php
+$input1 = $_POST["input1"];
+$input2 = $_POST["input2"]; 
+
+$output = $input1 + $input2;
+echo "The sum is: " .$output;
+?>
